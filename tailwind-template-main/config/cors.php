7195f29bf289ab54c0227e2f1c87de6c4fae3236
Narend-L <narendralintang@student.uns.ac.id>
2026-07-29 13:51:31 +0700

    Alhamdulillah

--- tailwind-template-main/config/cors.php
+++ tailwind-template-main/config/cors.php
@@ -15,14 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5174'),
-        'http://127.0.0.1:5174',
-    ],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174'],
 
     'allowed_origins_patterns' => [],
 
